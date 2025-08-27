b1fa0aacf9491d76188d6cad7a69cb5616dcae55
Gurekamjot003 <aroragurekam03@gmail.com>
2025-08-27 17:49:09 +0530

    fix - rotation bug fixes, modularized code for complexity reduction

--- Back end/Cube/Cube.hpp
+++ Back end/Cube/Cube.hpp
@@ -10,90 +10,62 @@ private:
     vector<vector<vector<Cubie*>>> cubies; 
 
     int n;
+
+    pair<int, int> get_abs_coordinates(Axis axis, int i, int j){
+        if(axis == Axis::X){
+            return {n-1-i, n-1-j};
+        }
+        else if(axis == Axis::Y){
+            return {j, i};
+        }
+        else if(axis == Axis::Z){
+            return {n-1-j, i};
+        }
+        return {};
+    }
+
+    void swap_cubies(Axis axis, int layer, int r_from, int c_from, int r_to, int c_to){
+        if(axis == Axis::X) swap(cubies[layer][r_from][c_from], cubies[layer][r_to][c_to]);
+        else if(axis == Axis::Y) swap(cubies[r_from][layer][c_from], cubies[r_to][layer][c_to]);
+        else if(axis == Axis::Z) swap(cubies[r_from][c_from][layer], cubies[r_to][c_to][layer]);
+    }
     
     void d1_transpose(Axis axis, int layer){
-        if(axis == Axis::X)
         for(int i = 0; i<(n-1); i++){
             for(int j = i+1; j<n; j++){
-                swap(cubies[layer][i][j], cubies[layer][j][i]);
-            }
-        }
-        else if(axis == Axis::Y)
-        for(int i = 0; i<(n-1); i++){
-            for(int j = i+1; j<n; j++){
-                swap(cubies[i][layer][j], cubies[j][layer][i]);
-            }
-        }
-        else if(axis == Axis::Z)
-        for(int i = 0; i<(n-1); i++){
-            for(int j = i+1; j<n; j++){
-                swap(cubies[i][j][layer], cubies[j][i][layer]);
+                auto[r1,c1] = get_abs_coordinates(axis, i, j);
+                auto[r2,c2] = get_abs_coordinates(axis, j, i);
+                swap_cubies(axis, layer, r1, c1, r2, c2);
             }
         }
     }
-
+    
     void d2_transpose(Axis axis, int layer){
-        if(axis == Axis::X)
         for(int i = 0; i<(n-1); i++){
             for(int j = (n-2-i); j>=0; j--){
-                swap(cubies[layer][i][j], cubies[layer][j][i]);
-            }
-        }
-        else if(axis == Axis::Y)
-        for(int i = 0; i<(n-1); i++){
-            for(int j = (n-2-i); j>=0; j--){
-                swap(cubies[i][layer][j], cubies[j][layer][i]);
-            }
-        }
-        else if(axis == Axis::Z)
-        for(int i = 0; i<(n-1); i++){
-            for(int j = (n-2-i); j>=0; j--){
-                swap(cubies[i][j][layer], cubies[j][i][layer]);
+                auto[r1,c1] = get_abs_coordinates(axis, i, j);
+                auto[r2,c2] = get_abs_coordinates(axis, n-1-j, n-1-i);
+                swap_cubies(axis, layer, r1, c1, r2, c2);
             }
         }
     }
 
     void row_reverse(Axis axis, int layer){
-        if(axis == Axis::X)
         for(int i = 0; i<n; i++){
             for(int j = 0; j<(n/2); j++){
-                swap(cubies[layer][i][j], cubies[layer][i][n-1-j]);
-            }
-        }
-        else if(axis == Axis::Y)
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<(n/2); j++){
-                swap(cubies[i][layer][j], cubies[i][layer][n-1-j]);
-            }
-        }
-        else if(axis == Axis::Z)
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<(n/2); j++){
-                swap(cubies[i][j][layer], cubies[i][n-1-j][layer]);
+                auto[r1,c1] = get_abs_coordinates(axis, i, j);
+                auto[r2,c2] = get_abs_coordinates(axis, i, n-1-j);
+                swap_cubies(axis, layer, r1, c1, r2, c2);
             }
         }
     }
 
     void column_reverse(Axis axis, int layer){
-        if(axis == Axis::X){
-            for(int j = 0; j<n; j++){
-                for(int i = 0; i<(n/2); i++){
-                    swap(cubies[layer][i][j], cubies[layer][n-1-i][j]);
-                }
-            }
-        }
-        else if(axis == Axis::Y){
-            for(int k = 0; k<n; k++){
-                for(int i = 0; i<(n/2); i++){
-                    swap(cubies[i][layer][k], cubies[n-1-i][layer][k]);
-                }
-            }
-        }
-        else if(axis == Axis::Z){
-            for(int j = 0; j<n; j++){
-                for(int i = 0; i<(n/2); i++){
-                    swap(cubies[i][j][layer], cubies[n-1-i][j][layer]);
-                }
+        for(int j = 0; j<n; j++){
+            for(int i = 0; i<(n/2); i++){
+                auto[r1,c1] = get_abs_coordinates(axis, i, j);
+                auto[r2,c2] = get_abs_coordinates(axis, n-1-i, j);
+                swap_cubies(axis, layer, r1, c1, r2, c2);
             }
         }
     }
